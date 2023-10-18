809c54ee5f1dbcc8b81f172caee4698faf6610d6
dnxtdev <dev@diamondnxt.com>
2023-10-18 19:01:35 +0200

    change dnft address

--- src/constants/addresses.js
+++ src/constants/addresses.js
@@ -1,6 +1,6 @@
 export const wbnb = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
 
-export const dnft = "0x97b984b5751009DeF5f50Fdc51477E3821B53050";
+export const dnft = "0x768C5e56F5d16fBb3Da359c78d423e8d3B182808";
 
 export const dnxttest = "0xd60F0D15d672Ec0B605B5Adc579f6b09BEe2F22c";
 export const dnxt = "0xd60F0D15d672Ec0B605B5Adc579f6b09BEe2F22c"//"0x0D74b7d0e373bA7E61758B0962f8F321Ac2b8387";
