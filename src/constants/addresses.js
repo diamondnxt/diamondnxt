3a01193e017dfcf7cf7adfbbe06306064719c482
Juandnxt <juan.henao@diamondnxt.com>
2023-11-10 11:44:15 +0000

    Update dnft address

--- src/constants/addresses.js
+++ src/constants/addresses.js
@@ -1,9 +1,9 @@
 export const wbnb = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
 
-export const dnft = "0x97b984b5751009DeF5f50Fdc51477E3821B53050";
+export const dnft = "0x2f13CcD3DaA5F8444957d8534A3a199C9a064198";
 
 export const dnxttest = "0xd60F0D15d672Ec0B605B5Adc579f6b09BEe2F22c";
-export const dnxt = "0xd60F0D15d672Ec0B605B5Adc579f6b09BEe2F22c"//"0x0D74b7d0e373bA7E61758B0962f8F321Ac2b8387";
+export const dnxt = "0x0D74b7d0e373bA7E61758B0962f8F321Ac2b8387";
 
 export const whitelist = "0x7C8b65CA927BBdbaf31026eddEa7B8226988b1eb";
 
